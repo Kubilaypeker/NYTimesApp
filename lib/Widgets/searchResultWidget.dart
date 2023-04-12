f002d89022c04b017bb68b27e0f6b3b61a3428ba
unknown <kubilaypeker0651@gmail.com>
2023-04-13 00:33:08 +0300

    unscrollable bug fixed

--- lib/Widgets/searchResultWidget.dart
+++ lib/Widgets/searchResultWidget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:otogootestapp/Screens/newsScreen.dart';
 
 class searchResult extends StatelessWidget {
   searchResult({Key? key, this.content, this.imageUrl, this.title}) : super(key: key);
@@ -13,7 +15,13 @@ class searchResult extends StatelessWidget {
             side: MaterialStateProperty.all(BorderSide(color: Colors.black12)),
             backgroundColor: MaterialStateProperty.all(Colors.grey.shade900.withOpacity(1))
         ),
-        onPressed: () {},
+        onPressed: () {
+          Get.to(newsScreen(
+            imageUrl: imageUrl,
+            caption: title,
+            content: content,
+          ));
+        },
         child: ListTile(
           leading: Image.network(imageUrl, fit: BoxFit.cover,),
           title: Text(title, style: GoogleFonts.poppins(color: Colors.white, fontWeight: FontWeight.w600),),
